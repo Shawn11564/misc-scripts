22b482a5b5b060fd7e90e88d562864f8c5ad8bc6
Shawn11564 <shawnmicheldev@gmail.com>
2024-11-23 16:27:04 -0500

    Add priviledged commands

--- base-manager.lua
+++ base-manager.lua
@@ -16,56 +16,103 @@ if not chat then
     error("Chat peripheral not found on " .. CONFIG.CHAT_PERIPHERAL_SIDE .. " side")
 end
 
--- Command handlers
-local commandHandlers = {
-    count = function(username, args)
-        if not args then
-            return "Usage: !count <item>"
-        end
-
-        local item = rs.getItem({ name = args })
-        if item ~= nil then
-            local count = item.amount
-            return "There are " .. count .. " " .. args .. " stored."
-        else
-            return "There are no " .. args .. " stored."
-        end
-    end,
-
-    craft = function(username, args)
-        if not args then
-            return "Usage: !craft <item> <count>"
-        end
-
-        -- split item and count args
-        local item = args:match("^(%S+)")
-        local toCraft = args:match("^%S+%s+(%d+)$")
-        
-        if not item or not toCraft then
-            return "Usage: !craft <item> <count>"
-        end
-
-        if not rs.isItemCraftable({ name = args }) then
-            return "There is no recipe for " .. args .. "."
-        end
-
-        local craftingStatus = rs.craftItem({ name = args, count = toCraft })
-        if craftingStatus == false then
-            return "There was an error crafting " .. args .. "."
-        else
-            return "Crafting " .. args .. " (" .. toCraft .. ")."
-        end
-    end,
-
-    -- Example of a command that doesn't need args
-    status = function(username)
-        return "System is running normally"
-    end,
-
-    -- Example of a command with multiple arguments
-    ping = function(username, args)
-        return "Pong! " .. (args or "")
+-- Helper function to parse item and count from args
+local function parseItemAndCount(args)
+    if not args then
+        return nil, nil
     end
+    
+    -- Match first word as item and second word as count
+    local item, count = args:match("^(%S+)%s+(%d+)$")
+    
+    -- If no count provided, just get the item
+    if not item then
+        item = args:match("^(%S+)$")
+        count = 1  -- Default to 1 if no count specified
+    else
+        count = tonumber(count)  -- Convert count to number
+    end
+    
+    return item, count
+end
+
+-- Command handlers with privilege flags
+local commandHandlers = {
+    count = {
+        privileged = true,
+        handler = function(username, args)
+            local item = args
+            if not item then
+                return "Usage: !count <item>"
+            end
+
+            local rsItem = rs.getItem({ name = item })
+            if rsItem ~= nil then
+                local count = rsItem.amount
+                return "There are " .. count .. " " .. item .. " stored."
+            else
+                return "There are no " .. item .. " stored."
+            end
+        end
+    },
+
+    craft = {
+        privileged = true,
+        handler = function(username, args)
+            local item, count = parseItemAndCount(args)
+            
+            if not item or not count then
+                return "Usage: !craft <item> <count>"
+            end
+
+            if not rs.isItemCraftable({ name = item }) then
+                return "There is no recipe for " .. item .. "."
+            end
+
+            local craftingStatus = rs.craftItem({ name = item, count = count })
+            if craftingStatus == false then
+                return "There was an error crafting " .. item .. "."
+            else
+                return "Crafting " .. count .. "x " .. item .. "."
+            end
+        end
+    },
+
+    status = {
+        privileged = false,
+        handler = function(username)
+            if rs then
+                return "System is running normally. RS Bridge connected."
+            else
+                return "System is running, but RS Bridge is not connected!"
+            end
+        end
+    },
+
+    help = {
+        privileged = false,
+        handler = function(username)
+            local public = {}
+            local privileged = {}
+            
+            for cmd, info in pairs(commandHandlers) do
+                if info.privileged then
+                    table.insert(privileged, CONFIG.COMMAND_PREFIX .. cmd)
+                else
+                    table.insert(public, CONFIG.COMMAND_PREFIX .. cmd)
+                end
+            end
+            
+            table.sort(public)
+            table.sort(privileged)
+            
+            local response = "Public commands: " .. table.concat(public, ", ")
+            if ALLOWED_USERS[username] then
+                response = response .. "\nPrivileged commands: " .. table.concat(privileged, ", ")
+            end
+            return response
+        end
+    }
 }
 
 -- Parse command and arguments from a message
@@ -73,41 +120,43 @@ local function parseCommand(message)
     if not message:match("^" .. CONFIG.COMMAND_PREFIX) then
         return nil
     end
-
+    
     local withoutPrefix = message:sub(#CONFIG.COMMAND_PREFIX + 1)
     local command = withoutPrefix:match("^(%S+)")
     if not command then
         return nil
     end
-
+    
     local args = withoutPrefix:match("^%S+%s+(.+)$")
     return command:lower(), args
 end
 
 -- Handle a chat message
 local function handleMessage(username, message)
-    if not ALLOWED_USERS[username] then
-        return
-    end
-
     local command, args = parseCommand(message)
     if not command then
         return
     end
-
-    local handler = commandHandlers[command]
-    if not handler then
-        local availableCommands = {}
-        for cmd in pairs(commandHandlers) do
-            table.insert(availableCommands, CONFIG.COMMAND_PREFIX .. cmd)
-        end
-        chat.sendMessage(string.format("Unknown command '%s'. Available commands: %s",
-            command,
-            table.concat(availableCommands, ", ")))
+    
+    local commandInfo = commandHandlers[command]
+    if not commandInfo then
+        chat.sendMessage(string.format("Unknown command '%s'. Use %shelp to see available commands.",
+            command, CONFIG.COMMAND_PREFIX))
         return
     end
-
-    local response = handler(username, args)
+    
+    -- Check privileges
+    if commandInfo.privileged and not ALLOWED_USERS[username] then
+        chat.sendMessage("You don't have permission to use this command.")
+        return
+    end
+    
+    local status, response = pcall(commandInfo.handler, username, args)
+    if not status then
+        chat.sendMessage("Error executing command: " .. tostring(response))
+        return
+    end
+    
     if response then
         chat.sendMessage(response)
     end
